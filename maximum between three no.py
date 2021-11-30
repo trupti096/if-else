9f7b240618372d4589b9965637f73e00b522dba2
trupti096 <84379470+trupti096@users.noreply.github.com>
2021-11-30 09:00:11 +0530

    Add files via upload

---
+++ maximum between three no.py
@@ -0,0 +1,9 @@
+num1=int(input("enter a no="))
+num2=int(input("enter a no="))
+num3=int(input("enter a no="))
+if num1>num2:
+    print(num1,"maximum no")
+elif num2>num3:
+    print(num2,"maximum no")
+else:
+    print(num3,"maximum no")
