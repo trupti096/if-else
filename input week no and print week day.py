a87cd92f9e00aad0cd9a9f74a3b4805099c95829
trupti096 <84379470+trupti096@users.noreply.github.com>
2021-11-30 08:59:14 +0530

    Add files via upload

---
+++ input week no and print week day.py
@@ -0,0 +1,17 @@
+day=int(input("enter a week no="))
+if day==1:
+    print("Sunday")
+elif day==2:
+    print("Monday")
+elif day==3:
+    print("Tuesday")
+elif day==4:
+    print("Wednesday")
+elif day==5:
+    print("Thursday")
+elif day==6:
+    print("Friday")
+elif day==7:
+    print("Saterday")
+else:
+    print("Nothing")
