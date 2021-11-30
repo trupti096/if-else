8cb06e15d9448f1654f05dbfe9973d051bc03390
trupti096 <84379470+trupti096@users.noreply.github.com>
2021-11-30 09:02:01 +0530

    Add files via upload

---
+++ no of days in that month.py
@@ -0,0 +1,27 @@
+day=int(input("enter any no="))
+if day==1:
+    print("January")
+elif day==2:
+    print("February")
+elif day==3:
+    print("March")
+elif day==4:
+    print("April")
+elif day==5:
+    print("May")
+elif day==6:
+    print("June")
+elif day==7:
+    print("July")
+elif day==8:
+    print("August")
+elif day==9:
+    print("September")
+elif day==10:
+    print("October")
+elif day==11:
+    print("November")
+elif day==12:
+    print("December")
+else:
+    print("Nothing")
