aabde207ed3359f0334f5d3528584068c9e5c8ff
trupti096 <84379470+trupti096@users.noreply.github.com>
2021-11-30 09:01:10 +0530

    Add files via upload

---
+++ no is divisible by 5 and 11 or not.py
@@ -0,0 +1,9 @@
+num=int(input("enterb a no="))
+if num%5:
+    print("no is divisible")
+elif num%11:
+    print("no is divisible")
+elif num%5 and num%11:
+    print("no is divisible")
+else:
+    print("no is divisible")
