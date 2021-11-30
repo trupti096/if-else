989f934a693b59687aca59e4a1046d452312281a
trupti096 <84379470+trupti096@users.noreply.github.com>
2021-11-30 09:04:49 +0530

    Add files via upload

---
+++ whether it is vowel or consonant.py
@@ -0,0 +1,5 @@
+ch=input("enter a character=")
+if ch=="a" or ch=="e" or ch=="i" or ch=="o" or ch=="u" and ch=="A" or ch=="E" or ch=="I" or ch=="O" or ch=="U":
+    print("it is a vowel")
+else:
+    print("it is a conconant")
