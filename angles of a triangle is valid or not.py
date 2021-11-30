cffdb512af211aead1bc90647da47eb297a3fe34
trupti096 <84379470+trupti096@users.noreply.github.com>
2021-11-30 08:54:27 +0530

    Add files via upload

---
+++ angles of a triangle is valid or not.py
@@ -0,0 +1,7 @@
+A=int(input("enter a 1st no="))
+B=int(input("enter a 2nd no="))
+C=int(input("enter a 3rd no="))
+if A+B+C==180:
+    print("triangle is valid")
+else:
+    print("triangle is not valid")
