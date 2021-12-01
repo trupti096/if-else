fe8e6cbc7cd7f3ccf67a8b58138b1c45f37dcef7
trupti096 <84379470+trupti096@users.noreply.github.com>
2021-12-01 08:42:25 +0530

    Add files via upload

---
+++ display the monument of that city.py
@@ -0,0 +1,9 @@
+city=input("enter city name=")
+if city == "Delhi":
+    print("red Fort")
+elif city == "Agra":
+    print("Taj Mahal")
+elif city == "Jaipur":
+    print("Jal Mahal")
+else:
+    print("Nothing")
